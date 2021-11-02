11d8e5b1b648cc6d820f65f20e0eb8b06fe5907e
Mario RIvas <mariojrivas@outlook.com>
2021-11-01 21:59:53 -0700

    add
    added working scripts

---
+++ General_Use_Scripts/Linux_Basics_for_Hackers_notes.md
@@ -0,0 +1,40 @@
+//Chapter 1 Basics
+
+CTRL + ALT + T = Open New terminal in RDP on Kali Linux box
+
+pwd = finding self in directory
+
+whoami = returns the account using system currently
+
+cd = change directory
+
+ls  = listing contemts of directory
+ls -la = all files even hidden
+
+--help -h = getting help on any command
+
+man = command before any command to ask for manual
+
+locate = finds files by searching db on server updated once a day
+
+whereis = searches  for biunary files and source and man page if possible, not just looks for the words "apple" searches for program binaries not just anything with apple in name
+
+which = returns location of binaries in PATH variable in linux
+
+find = looks through entire directoy
+find directory options expression | find directory [FILE TYPE] -name [FOO]
+
+* = is the wildcard operator
+
+grep = used in piping commonly
+
+ps = command used to find processes running on server
+ps aux | grep apache2
+
+cat = concatentate,display contents of file or make small files
+
+> is a redirect, CTRL + D to exit, when pressed enter linux goes to interactive mode
+>> double is to append
+> before cat overrides the file info
+
+touch
---
+++ General_Use_Scripts/WindeskVMConnect.ps1
@@ -0,0 +1,69 @@
+cd "C:\Users\V-MARIORIVAS\Work Folders\Desktop\PublicSecurity"
+
+Connect-AzAccount 
+
+Select-AzSubscription -Subscription 851a47f9-7201-4e7e-aecd-09ce41c0a124 -Name "ResumeChallenge" -Force
+$VmStatus = Get-AzVM -ResourceGroupName WINDESKRG -Name windeskvm01 -Status
+
+#$VmStatus
+
+$VmDisplayStatus = $VmStatus.Statuses[1].DisplayStatus
+
+#Write-Host $VmDisplayStatus
+
+Write-Host "Status of VM is" "$VmDisplayStatus `n" -foregroundcolor green 
+
+#IfElse loop to check if VM is running#
+
+if ($VmDisplayStatus -eq "VM running")
+{
+    Write-Host "VM is running `n" -foregroundcolor green   
+
+}
+elseif ($VmDisplayStatus -eq "VM deallocated")
+{
+    Write-Host "Vm is being Started `n" -foregroundcolor green 
+    Start-AzVM -ResourceGroupName WINDESKRG -Name windeskvm01
+    Write-Host "Vm Started `n" -foregroundcolor green 
+}
+
+$key = Get-AzKeyVaultSecret -VaultName Windeskkeyvault -SecretName windeskvmpassword -AsPlainText
+
+$admin = Get-AzKeyVaultSecret -VaultName Windeskkeyvault -SecretName windeskvmadminusername -AsPlainText
+
+$GetAzPubAddress = Get-AzPublicIpAddress -Name windeskvm01-ip
+
+$VMIPAddress = $GetAzPubAddress.IpAddress
+
+cmdkey /generic:TERMSRV/"$VMIPAddress" /user:"$admin" /pass:"$key"
+
+#mstsc /v:"$VMIPAddress"
+
+$CurrentConnectionStatus = netstat -ano | Where-Object { $_ -match $VMIPAddress} | ConvertFrom-String
+
+Write-Host "Getting NetStat information `n"
+
+$CurrentConnectionStatusBoolean = $null
+
+$CurrentConnectionStatusBoolean = if ($CurrentConnectionStatus) { 'True' } else { 'False' }
+
+Write-Host "Determing if connection is active `n"
+
+if ($CurrentConnectionStatusBoolean -eq "True")
+{   
+    Write-Host "Connection currently established, deleting connection stored on device `n"
+    cmdkey /delete:"$VMIPAddress"
+}
+elseif ($CurrentConnectionStatusBoolean -eq "False")
+{
+    Write-Host "Connection starting `n"
+    mstsc /v:"$VMIPAddress"
+    Write-Host "Self deleting key after use `n"
+    cmdkey /delete:"$VMIPAddress"
+}
+
+
+#netstat -n | find "$VMIPAdress" | find "ESTABLISHED"
+
+#netstat -ano | findstr ":3389"
+
