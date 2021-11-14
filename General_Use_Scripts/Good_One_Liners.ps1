78c4700b3462cff1364fb6e0c9293f72c24c7482
Mario RIvas <mariojrivas@outlook.com>
2021-11-14 11:23:47 -0800

    Update Good_One_Liners.ps1

--- General_Use_Scripts/Good_One_Liners.ps1
+++ General_Use_Scripts/Good_One_Liners.ps1
@@ -1,1 +1,3 @@
 ﻿Get-ChildItem | Rename-Item -NewName {$_.Name -replace "[()]", ""}
+
+Get-ChildItem -Path "C:\Users\azadmin\Calibre Library\*" -Include *.mobi -Recurse | Copy-Item -Destination "G:\My Drive\EBOOKS\MOBI"
