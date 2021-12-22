4abbb120f4c36fc849be3610d6e40e8d7f7b98f8
RivasMario <mariorjivas@outlook.com>
2021-12-22 03:20:52 -0800

    fsddf

--- Good_One_Liners.ps1
+++ Good_One_Liners.ps1
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 ﻿Get-ChildItem | Rename-Item -NewName {$_.Name -replace "[(]z-lib.org[])]", ""}
-=======
 ﻿Get-ChildItem | Rename-Item -NewName {$_.Name -replace "[(z-lib.org)]", ""}
->>>>>>> e509bfb6f0a36e7f285f0416a0689ac013991ccc
 
 lsusb 
 
@@ -15,3 +12,5 @@ PacketTracer software
 Get-ChildItem -Path "C:\Users\azadmin\Calibre Library\*" -Include *.mobi -Recurse | Copy-Item -Destination "G:\My Drive\EBOOKS\MOBI"
 
 kubectl get pods | grep fdv2-6| awk '{print $1}' | while read -r line ; do kubectl exec -i $line -- powershell  -c "netsh http show sslcert hostnameport=aadcdn.msftauthimages.us:443;" </dev/null; done
+
+echo "cd ~/Desktop/Java\ Files" >> ~/.bashrc
