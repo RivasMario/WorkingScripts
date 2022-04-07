efff3646e9a58ede4c86181d3033711b5ae8bdeb
Mario RIvas <mariojrivas@outlook.com>
2022-04-07 11:05:34 -0700

    qwerty

--- Powershell/For_Loops.ps1
+++ Powershell/For_Loops.ps1
@@ -27,12 +27,58 @@ for (; $outside -lt 15; $outside++) {
 }
 
 #Looping through array
-$pets - @("Cat", "Dog", "Fish", "Turtle")
+$pets = @("Cat", "Dog", "Fish", "Turtle")
 $pets.Count
 
-$pets[2]
+$pets[2] 
 
 "My pets in order:"
 for ($i = 0; $i -lt $pets.Count; $i++) {
     $pets[$i]
+}
+
+#How about if i'd like to add a numbered list
+
+Write-Output "My pets in order:`n"
+
+for ($i = 0; $i -lt $pets.Count; $i++) {
+    $Number = $i +1
+    "  {0}. {1}" -f $Number, $pets[$i]
+}
+
+# Can also be used to count strings
+
+for ($text = ''; $text.Length -lt 10; $text += '@') {
+    $text
+}
+
+# Nested for, grid situation where they are printed
+$row = 1..10
+$column = 1..10
+
+for ($i = 0; $i -lt $row.Count; $i++) {
+    for ($j = 0; $j -lt $column.Count; $j++) {
+
+        $r = $row[$i]
+        $c = $column[$j]
+        $result = $r * $c
+
+        $t = "{0} * {1} = {2}" -f $r, $c, $result
+        Write-Host $t
+    }
+}
+
+#never ending script have to do an interrupt CTRL + C
+#didn't specify condition so it goes on forever
+for ( $i = 0; ; $i++) {
+    "Loop number: $i"
+}
+
+#We can write a condition inside the loop to break out
+for ( $i = 0; ; $i++) {
+    "Loop number: $i"
+    if ($i -eq 1000) {
+        "Break out at: {0}" -f $i 
+        break
+    }
 }
