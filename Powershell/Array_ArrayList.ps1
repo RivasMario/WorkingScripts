e0a67f64785bdeff89a2c3947306adb1092fcaee
Mario RIvas <mariojrivas@outlook.com>
2022-04-08 13:59:21 -0700

    updates on notes

---
+++ Powershell/Array_ArrayList.ps1
@@ -0,0 +1,13 @@
+#link to the video:
+
+#Documentation
+
+#What is an array and why is it needed?
+
+<#
+Array is a data structure that allows you to collect multiple item under one collection
+so the items can be accessed at will, iterated over, and updated as need be
+
+Arrays allow you to work with a bunch of variables in a more human readable form
+#>
+
