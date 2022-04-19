35aaea4ffb2888da7fba3d1c5117428408e1acb4
RivasMario <mariojrivas@outlook.com>
2022-04-18 19:35:28 -0700

    qwerty

---
+++ Powershell/Disable_Smartcard.ps1
@@ -0,0 +1,97 @@
+
+enter-pssession -computername PHX20AZFUTLA05.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA07.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA09.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA11.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA17.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA19.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA21.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA23.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername PHX20AZFUTLA25.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA02.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA06.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA08.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA10.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA12.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA18.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA20.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA22.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA24.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+enter-pssession -computername SN5AZFUTLA26.USOE.GBL
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $txtComputer.Text)
+$regKey = $reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\policies\\system", $true)
+$regKey.SetValue("scforceoption", 0)
+exit
+
