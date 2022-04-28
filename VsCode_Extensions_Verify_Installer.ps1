7fcad51615155745baa038750b17cee2b79c7158
RivasMario <mariojrivas@outlook.com>
2022-04-27 18:00:46 -0700

    adding project script to add VScode & Extensions to SAW  host

---
+++ VsCode_Extensions_Verify_Installer.ps1
@@ -0,0 +1,8 @@
+<#
+7/27/2022 Mario Rivas
+
+Script checks if Software center has VScode installed, then checks which extensions are available. Later adds extensions on list to VSCode instance.
+
+Best used for SAW on MSFT corp machines. 
+#>
+
