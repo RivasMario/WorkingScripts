12d864d790d9f24257584d37f66245c1b24d70ae
Mario Rivas <mariojrivas@outlook.com>
2023-12-12 05:09:04 +0000

    readability changes

--- PS_IDENTITY/Get-BladeSetttingsConfirmation.ps1
+++ PS_IDENTITY/Get-BladeSetttingsConfirmation.ps1
@@ -13,13 +13,13 @@ Function Get-ServerSettingsVerification ($servername){
     $lp = $ht.NumberOfLogicalProcessors
     $coresum = 0
     $lpsum = 0
-    $cores | %{$coresum += $_}
-    $lp | %{$lpsum += $_}
+    $cores | ForEach-Object{$coresum += $_}
+    $lp | ForEach-Object{$lpsum += $_}
     Write-Host "Number of cores: $coresum"
     Write-Host "Number of Logical Processors: $lpsum"
 
     Write-Host "`nEnabled Power Plan" -ForegroundColor Cyan
-    $pp = Get-WmiObject -Class win32_powerplan -Namespace root\cimv2\power -ComputerName $servername -Filter "isActive='true'" | Select ElementName 
+    $pp = Get-WmiObject -Class win32_powerplan -Namespace root\cimv2\power -ComputerName $servername -Filter "isActive='true'" | Select-Object ElementName 
     $pp.ElementName
 
     $reg0 = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]"LocalMachine", $servername)
