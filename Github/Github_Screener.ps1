009a957417f05059e7b3ab5a1a28a369f92cdf70
Mario Rivas <mariojrivas@outlook.com>
2024-04-05 20:40:35 +0000

    Reformatting Naming Conventions, extracting files to different repos

--- Github/Github_Screener.ps1
+++
@@ -1,9 +0,0 @@
-$url = "https://esescreener.azurewebsites.net/api/screener/submit/39f26a498befe029f3436cf2db6583c2"
-
-$body = @{
-    "name" = "Mario Rivas"
-    "favorite" = "ls -a"
-    "code"= "Nzk0ODc4N2I4NjYzZjhiNDViMjFiMWJmMjUxODNlMDcxODY5OTZhMz1leHBpcmVzLTE2MTg1MDU5MjI4MDM7RXhwaXJlcz1UaHUsIDE1IEFwciAyMDIxIDE2OjU4OjQyIEdNVDtMYW5nPUVO"
-}
-
-Invoke-RestMethod -Method 'POST' -Uri $url -Body ($body|ConvertTo-Json) -ContentType "application/json"
